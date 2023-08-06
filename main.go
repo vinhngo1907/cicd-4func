f03f6f80aa47ab2cccf61219c1d41fc6c1d64365
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-06 21:29:51 +0700

    commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+
+	"github.com/bootdotdev/learn-cicd-starter/internal/database"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type api config struct{
+	DB *database.Queries
+}
